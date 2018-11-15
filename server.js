26a8397cf1d2bb59d8cd1bc194f64b08a31d7c26
jared <Astropig15@gmail.com>
2018-11-14 20:41:45 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+var app = express();
+
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/html'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url} ${req.ip}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err){
+            console.log('unable to append');
+        }
+    });
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Welcome Page',
+        welcomeMessage: 'Welcome to my page yeet',
+        currentYear: new Date().getFullYear()
+    });
+});
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'ABout Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessege: 'bad request'
+    });
+});
+app.listen(port, () => {
+    console.log(`Server is up on port ${port.toString()}`);
+});
